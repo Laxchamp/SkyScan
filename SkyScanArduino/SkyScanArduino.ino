3de6f580886e79d52c5469fcda76f2a46960f779
Rowe <coledrowe@gmail.com>
2026-02-24 13:35:42 -0500

    Test Of new Arduino non blink

--- SkyScanArduino/SkyScanArduino.ino
+++ SkyScanArduino/SkyScanArduino.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+By: Cole Rowe 
+Date: 2.24.26 
+ 
+int laserPin = 10; 
+int sensorPin = 6; 
+int ledPin = 11; 
+ 
+void setup() { 
+  pinMode(laserPin, OUTPUT); 
+  pinMode(sensorPin, INPUT); 
+  pinMode(ledPin, OUTPUT); 
+ 
+  digitalWrite(laserPin, HIGH); // turn laser ON 
+} 
+ 
+void loop() { 
+  int sensorState = digitalRead(sensorPin); 
+ 
+  if (sensorState == LOW) { 
+    // Laser beam broken → flicker LED 
+    digitalWrite(ledPin, HIGH); 
+    delay(200); 
+    digitalWrite(ledPin, LOW); 
+    delay(200); 
+  } else { 
+    // Laser intact → LED off 
+    digitalWrite(ledPin, LOW); 
+  } 
